0b4d0b64f44b92c47f7069775db33c3ceb2e986b
Matthew Jou <joum@Matthews-MacBook-Air.local>
2024-11-22 14:47:05 -0800

    add ModelAdmin and InlineModelAdmin

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from blogging.models import Post, Category
+from blogging.models import Post, Category, PostAdmin, CategoryAdmin
 
-admin.site.register(Post)
-admin.site.register(Category)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
--- blogging/models.py
+++ blogging/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib import admin
+
 
 class Post(models.Model):
     title = models.CharField(max_length=128)
@@ -23,3 +25,25 @@ class Category(models.Model):
     
     class Meta:
         verbose_name_plural = 'Categories' 
+
+
+# TODO: Create an InlineModelAdmin to represent Categories on the Post admin view.
+class CategoryInline(admin.TabularInline):
+    model = Category.posts.through
+
+
+# TODO: Create a ModelAdmin class for the Post model
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_date', 'published_date')
+    list_filter = ('published_date', 'author')
+    search_fields = ('title', 'text')
+    inlines = [CategoryInline]
+
+
+# TODO: Create a ModelAdmin class for Category model
+# exclude the ‘posts’ field from the form in your Category admin
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    search_fields = ('name', 'description')
+    exclude = ('posts',)
+
