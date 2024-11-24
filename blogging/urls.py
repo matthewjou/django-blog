51d36a9965f174d62001331466eb0fc838726c69
Matthew Jou <joum@Matthews-Air.home>
2024-11-23 19:18:21 -0800

    convert blog function views to class views

--- blogging/urls.py
+++ blogging/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from blogging.views import list_view, detail_view
+from blogging.views import list_view, detail_view, BlogListView, BlogDetailView
 
 urlpatterns = [
-    path('posts/<int:post_id>/', detail_view, name="blog_detail"),
-    path('', list_view, name="blog_index"),
+    # old views
+    # path('posts/<int:post_id>/', detail_view, name="blog_detail"),
+    # path('', list_view, name="blog_index"),
+    path("posts/<int:pk>/", BlogDetailView.as_view(), name="blog_detail"),
+    path("", BlogListView.as_view(), name="blog_index"),
 ]
